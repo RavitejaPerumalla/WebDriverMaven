420acffa474abb6bf2fbecf367789bf09db9f0f6
Raviteja Perumalla <ravitejasasurie@gmail.com>
2020-11-05 10:33:41 +0530

    Commiting the EGit Changes

--- src/test/java/webdriverMaven/LoginTest.java
+++ src/test/java/webdriverMaven/LoginTest.java
@@ -26,6 +26,7 @@ public class LoginTest {
 	@Test
 	public void doLogin() {
 		
+		//Commiting the EGit Changes
 		driver.get("http://gmail.com");
 		driver.manage().window().maximize();
 		WebDriverWait wait = new WebDriverWait(driver, 5);
